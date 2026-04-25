56596af0e4e9ccf1815d9faa9e8e1b1a535f9da3
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-26 01:37:43 +0900

    인증 아키텍처 : 인증 프로세스 CustomAuthenticationProvider 구현

--- src/main/java/com/b1uffer/securitymvc/auth/CustomAuthenticationProvider.java
+++ src/main/java/com/b1uffer/securitymvc/auth/CustomAuthenticationProvider.java
@@ -1,4 +1,29 @@
 package com.b1uffer.securitymvc.auth;
 
-public class CustomAuthenticationProvider {
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+import java.util.ArrayList;
+
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        // 커스텀 인증 로직 구현하기
+        if("customUser".equals(username) && "1234".equals(password)) {
+            return new UsernamePasswordAuthenticationToken(username, password, new ArrayList<>());
+        } else {
+            throw new BadCredentialsException("인증 실패");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
 }
