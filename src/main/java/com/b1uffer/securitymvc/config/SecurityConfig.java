9aab17cd58dd56e96908800f0992089b5fec73ce
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-25 20:37:54 +0900

    SecurityConfig MVC 환경 FilterChain 구현

--- src/main/java/com/b1uffer/securitymvc/config/SecurityConfig.java
+++ src/main/java/com/b1uffer/securitymvc/config/SecurityConfig.java
@@ -1,4 +1,25 @@
 package com.b1uffer.securitymvc.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .anyRequest().authenticated())
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers
+                        .frameOptions(frame -> frame.sameOrigin()))
+                .formLogin(login -> login.permitAll());
+
+        return http.build();
+    }
 }
