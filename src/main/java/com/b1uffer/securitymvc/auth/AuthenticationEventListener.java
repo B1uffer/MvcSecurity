f1f9a0c8b05d03007d94b4688af8a25d3943a961
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-26 01:39:22 +0900

    인증 아키텍처 : 인증 프로세스 AuthenticationEventListener 구현

--- src/main/java/com/b1uffer/securitymvc/auth/AuthenticationEventListener.java
+++ src/main/java/com/b1uffer/securitymvc/auth/AuthenticationEventListener.java
@@ -1,4 +1,19 @@
 package com.b1uffer.securitymvc.auth;
 
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+
+@Component
 public class AuthenticationEventListener {
+    @EventListener
+    public void handleSuccess(AuthenticationSuccessEvent event) {
+        System.out.println("로그인 성공 : " + event.getAuthentication().getName());
+    }
+
+    @EventListener
+    public void handleFailure(AuthenticationFailureBadCredentialsEvent event) {
+        System.out.println("로그인 실패 : " + event.getException().getMessage());
+    }
 }
