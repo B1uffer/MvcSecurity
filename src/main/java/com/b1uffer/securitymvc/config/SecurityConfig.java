49a84aa18da93fcd060acdf487b4fa99d66765b8
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-28 16:50:05 +0900

    인가 아키텍처 : 인가 프로세스 requestMatchers 추가

--- src/main/java/com/b1uffer/securitymvc/config/SecurityConfig.java
+++ src/main/java/com/b1uffer/securitymvc/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.b1uffer.securitymvc.config;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,6 +15,8 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .requestMatchers("/admin/**").access(AuthorityAuthorizationManager.hasRole("ADMIN"))
+                        .requestMatchers("/user/**").access(AuthorityAuthorizationManager.hasRole("USER"))
                         .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
                 .headers(headers -> headers
