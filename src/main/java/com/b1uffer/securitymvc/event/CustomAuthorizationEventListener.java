798f6dcb4813a90e98ec58edf8c9fe9ea6d0eac6
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-29 00:31:46 +0900

    인가 아키텍처 : 인가 프로세스 AuthorizationEvent 발행과 구독 CustomAuthorizationEventListener 구현

--- src/main/java/com/b1uffer/securitymvc/event/CustomAuthorizationEventListener.java
+++ src/main/java/com/b1uffer/securitymvc/event/CustomAuthorizationEventListener.java
@@ -1,4 +1,13 @@
 package com.b1uffer.securitymvc.event;
 
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authorization.event.AuthorizationEvent;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CustomAuthorizationEventListener {
+    @EventListener
+    public void handleAuthEvent(AuthorizationEvent event) {
+        System.out.println("인가 이벤트 발생 : " + event);
+    }
 }
