cd433c041afa4f3610d084ed3893a68604d7047c
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-01 01:24:22 +0900

    커스텀 필터 구현 : 구현 방법 CustomGenericFilter 실무 활용 예, 전처리

--- src/main/java/com/b1uffer/securitymvc/custom/CustomGenericFilter.java
+++ src/main/java/com/b1uffer/securitymvc/custom/CustomGenericFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.ServletResponse;
 import org.springframework.web.filter.GenericFilterBean;
 
 import java.io.IOException;
+import java.util.List;
 
 public class CustomGenericFilter extends GenericFilterBean { // GenericFilterBean을 extends 한 다음
     // doFilter 메서드만 구현하면 됨
@@ -16,6 +17,15 @@ public class CustomGenericFilter extends GenericFilterBean { // GenericFilterBea
                          FilterChain filterChain) throws IOException, ServletException {
         // 요청 전 처리 로직
         System.out.println("[CustomGenericFilter] 요청 처리 전 실행하기");
+
+        // 전처리
+        String clientIp = servletRequest.getRemoteAddr();
+        List<String> whiteList = List.of("127.0.0.1", "192.168.0.10");
+        if(!whiteList.contains(clientIp)) {
+            servletResponse.setContentType("text/plain;charset=utf-8");
+            servletResponse.getWriter().write("허용되지 않은 IP입니다.");
+            return; // 꼭 해줘야함, 필터 체인 중단
+        }
 
         // 필터 체인 진행
         filterChain.doFilter(servletRequest, servletResponse);
