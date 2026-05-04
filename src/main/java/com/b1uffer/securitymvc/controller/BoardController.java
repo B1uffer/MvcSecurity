4c6f5792ebf6c6e44f154ae3eb46cc495894fb19
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-05 04:25:23 +0900

    XSS(Cross-Site Scripting) BoardController API 구현

--- src/main/java/com/b1uffer/securitymvc/controller/BoardController.java
+++ src/main/java/com/b1uffer/securitymvc/controller/BoardController.java
@@ -1,4 +1,26 @@
 package com.b1uffer.securitymvc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class BoardController {
+    private final List<String> posts = new ArrayList<>();
+
+    @GetMapping("/board")
+    public String board(Model model) {
+        model.addAttribute("posts", posts);
+        return "board";
+    }
+
+    @GetMapping("/board")
+    public String addPost(@RequestParam String content) {
+        posts.add(content);
+        return "redirect:/board";
+    }
 }
