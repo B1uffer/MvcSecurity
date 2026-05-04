cc753b72c5646e346b1dca06afd272565729d65b
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-05 04:31:21 +0900

    XSS(Cross-Site Scripting) 애너테이션 수정

--- src/main/java/com/b1uffer/securitymvc/controller/BoardController.java
+++ src/main/java/com/b1uffer/securitymvc/controller/BoardController.java
@@ -3,6 +3,8 @@ package com.b1uffer.securitymvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -18,7 +20,7 @@ public class BoardController {
         return "board";
     }
 
-    @GetMapping("/board")
+    @PostMapping("/board")
     public String addPost(@RequestParam String content) {
         posts.add(content);
         return "redirect:/board";
